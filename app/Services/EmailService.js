0ea62311604a38713fae7f2212f9e11f6c49cf0a
tuananhzippy <kieutuananh1995@gmail.com>
2022-07-11 14:29:43 +0700

    optimize: email service

--- app/Services/EmailService.js
+++ app/Services/EmailService.js
@@ -1,46 +1,41 @@
 'use strict'
 
 const Mail = use('Mail');
-const Config = use('Config');
 const ScheduleData = use('App/Models/Schedule');
 
 class EmailService {
 
     async sendMail(scheduleId, statusCode, body) {
-        var currentTime = new Date();
-        var scheduleInfo = await ScheduleData.find(scheduleId);
-        if(scheduleInfo.last_time) {
-            var lastTime = new Date(scheduleInfo.last_time);
-        }
-        if ((scheduleInfo.emails && scheduleInfo.emails != '') || Config.get('mail.send') === "on") {
-            if (!scheduleInfo.last_time || currentTime.getTime() >= lastTime.getTime() + 60 * 60 * 1000) {
-                let htmlTeplate = `<ul style="padding-left: 0px">`;
-                htmlTeplate += `<li>URL: ${scheduleInfo.url}</li>`;
-                htmlTeplate += `<li>Note: ${scheduleInfo.note}</li>`;
-                htmlTeplate += `<li>Status Code: ${statusCode}</li>`;
-                htmlTeplate += `<li>Body: ${body}</li>`;
-                htmlTeplate += "</ul>";
-                scheduleInfo.last_time = currentTime.getDateTime();
-                await scheduleInfo.save();
+        let scheduleInfo = await ScheduleData.find(scheduleId);
+        if (scheduleInfo.emails && scheduleInfo.emails != '') {
+            let htmlTeplate = `
+                <ul style="padding-left: 0px">
+                    <li>URL: ${scheduleInfo.url}</li>
+                    <li>Note: ${scheduleInfo.note}</li>
+                    <li>Status Code: ${statusCode}</li>
+                    <li>Body: ${body}</li>
+                </ul>`;
 
-                var result = await Mail.raw(htmlTeplate, (message) => {
-                    let receivers = [];
-                    if (scheduleInfo.emails && scheduleInfo.emails != '') {
-                        receivers = scheduleInfo.emails.split(',').map(function (item) { return {email: item.trim(), name: 'Notifications'} });
-                    } else {
-                        receivers = Config.get('mail.receivers');
-                    }
-                    receivers.forEach((item) => {
-                        message.to(item.email, item.name);
-                    });
-                    message.from('no-reply@monitor.megaads.vn', 'Mega Schedule').subject('Request Error - Mega Schedule')
-                }).catch(error => {
-                    console.error(error);
+            scheduleInfo.last_time = new Date().getDateTime();
+            await scheduleInfo.save();
+
+            let result = await Mail.raw(htmlTeplate, (message) => {
+                let receivers = scheduleInfo.emails.split(',').map(item => { 
+                    return {
+                        email: item.trim(), 
+                        name: 'Notifications'
+                    };
                 });
-                return result;
-            }
+                receivers.forEach(item => {
+                    message.to(item.email, item.name);
+                });
+                message.from('no-reply@monitor.megaads.vn', 'Mega Schedule').subject('Request Error - Mega Schedule')
+            }).catch(error => {
+                console.error(error);
+            });
+
+            return result;
         }
-        
     }
 
 }
